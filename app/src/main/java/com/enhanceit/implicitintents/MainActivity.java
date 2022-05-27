e94ca750c07e176d6c1dfd286f9d5925fef42336
zayelmech <i.equipo001@gmail.com>
2022-05-27 16:53:13 -0400

    Task 1: Create the project and layout

---
+++ app/src/main/java/com/enhanceit/implicitintents/MainActivity.java
@@ -0,0 +1,24 @@
+package com.enhanceit.implicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void openWebsite(View view) {
+    }
+
+    public void openLocation(View view) {
+    }
+
+    public void shareText(View view) {
+    }
+}
