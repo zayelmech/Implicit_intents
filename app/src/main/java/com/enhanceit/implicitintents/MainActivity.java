4e3f910291ae7bdfc80312fdf62b9b2af8e8a202
zayelmech <i.equipo001@gmail.com>
2022-05-27 17:05:45 -0400

    4. Task 2: Implement the Open Website button

--- app/src/main/java/com/enhanceit/implicitintents/MainActivity.java
+++ app/src/main/java/com/enhanceit/implicitintents/MainActivity.java
@@ -2,21 +2,44 @@ package com.enhanceit.implicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText mWebsiteEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mWebsiteEditText = findViewById(R.id.website_edittext);
+
     }
 
     public void openWebsite(View view) {
+        // Get the URL text.
+        String url = mWebsiteEditText.getText().toString();
+
+        // Parse the URI and create the intent.
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+
+        // Find an activity to hand the intent and start that activity.
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+
+        } else {
+        Log.d("ImplicitIntents", "Can't handle this!");
     }
 
+}
+
     public void openLocation(View view) {
+
     }
 
     public void shareText(View view) {
